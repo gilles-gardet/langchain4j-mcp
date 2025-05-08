1264b9677e5e783e685f63f5ca9ec4cdcf8d08d4
Gilles <gilles.gardet@gmail.com>
2025-05-08 15:35:09 +0200

    chore: init the springboot project

---
+++ src/test/kotlin/com/ggardet/langchain/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ggardet.langchain.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
